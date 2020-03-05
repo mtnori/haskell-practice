45cbb45d6ebbac9debea76c7f9392a628c346a7a
mtnori <moto44.noriyuki@gmail.com>
2020-03-05 22:39:17 +0900

    基本クラス

--- src/Test.hs
+++ src/Test.hs
@@ -1,6 +1,38 @@
 module Test where
 
-double x = x + x
 quadruple x = double (double x)
 factorial n = product [1..n]
 average ns = sum ns `div` length ns
+
+bools :: [Bool]
+bools = [True, False]
+
+nums :: [[Int]]
+nums = [[1,2],[3,4]]
+
+add :: Int -> Int -> Int -> Int
+add x y z = x + y + z
+
+copy :: a -> (a,a)
+copy x = (x, x)
+
+apply :: (a -> b) -> a -> b
+apply f x = f x
+
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (b, a) -> (a, b)
+swap (x, y) = (y, x)
+
+pair :: a -> b -> (a, b)
+pair x y = (x, y)
+
+double :: Num a => a -> a
+double x = x * 2
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (a -> a) -> a -> a
+twice f x = f (f x)
